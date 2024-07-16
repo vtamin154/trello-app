33f469a395568d086b2864a7d24d97bd93de3cef
vtamin154 <vtamin154@gmail.com>
2024-07-17 00:07:29 +0700

    add new ripple effect

--- src/app/pages/boards/board-bar/board-bar.component.ts
+++ src/app/pages/boards/board-bar/board-bar.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-board-bar',
   templateUrl: './board-bar.component.html',
-  styleUrls: ['./board-bar.component.scss']
+  styleUrls: ['./board-bar.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class BoardBarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  //create ripple effect for button
+  createRipple(event: MouseEvent) {
+    const button = event.currentTarget as HTMLElement;
 
-  ngOnInit(): void {
+    const circle = document.createElement('span');
+
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+    circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+    circle.classList.add('ripple');
+
+    const ripple = button.getElementsByClassName('ripple')[0];
+
+    if (ripple) {
+      ripple.remove();
+    }
+
+    button.appendChild(circle);
   }
 
+  ngOnInit(): void {}
 }
